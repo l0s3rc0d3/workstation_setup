9b594328a7af7caef03342c22c43008555a90427
Giorgio Scampini <scampini.gio@gmail.com>
2026-05-13 13:54:33 +0200

    fix(macos_config): bugfix setup.sh p10k theme

--- macos_config/setup.sh
+++ macos_config/setup.sh
@@ -452,7 +452,7 @@ fi
 [[ -f ~/.zsh_user ]] && source ~/.zsh_user
 
 # ── Powerlevel10k config ──────────────────────────────────────────────────────
-[[ -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
 ZSHRC
 
 # ── .zsh_user ─────────────────────────────────────────────────────────────
